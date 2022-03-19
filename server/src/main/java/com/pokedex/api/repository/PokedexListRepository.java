1447df54e508e834a20d5b9611b72fae36e2b8f9
ogatatsu <55011568+OGTuw@users.noreply.github.com>
2022-03-19 23:15:43 +0900

    Changes

--- server/src/main/java/com/pokedex/api/repository/PokedexListRepository.java
+++ server/src/main/java/com/pokedex/api/repository/PokedexListRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PokedexListRepository extends JpaRepository<PokemonList, Integer> {
-    @Query(value = "select pokemon.id, pokemon.name, types.type, pokemon_types.slot "
+    @Query(value = "select pokemon.id, pokemon.name, types.type "
         + "from pokemon, pokemon_types, types "
         + "where pokemon.id = pokemon_types.pokemon_id AND pokemon_types.type_id = types.id "
+            + "AND(pokemon_types.slot = 1 OR pokemon_types.slot = 2)"
         + "order by pokemon.id ",
         nativeQuery = true)
     List<PokemonList> findPokemonList();
